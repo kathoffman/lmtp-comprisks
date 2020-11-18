f1220e00f06e5d0e6065c6905da7e3c4f2a08996
Nick Williams <ntwilliams.personal@gmail.com>
2020-11-18 16:17:17 -0500

    only necessary columns passed to super learner prediction

--- R/sl.R
+++ R/sl.R
@@ -1,4 +1,3 @@
-
 run_ensemble <- function(Y, X, learners, outcome_type, id) {
   family <- ifelse(outcome_type == "binomial", binomial(), gaussian())
   SuperLearner::SuperLearner(Y, X, family = family[[1]], SL.library = learners,
